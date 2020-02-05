ad18fcb2cccce9a3ce8dbc2b7263af9bbf0ef508
Julio Nicolas <jnicolas.marcos@gmail.com>
2020-02-05 14:56:27 -0600

    Initial commit

---
+++ KnobControlJN/KnobControlJN.h
@@ -0,0 +1,19 @@
+//
+//  KnobControlJN.h
+//  KnobControlJN
+//
+//  Created by Julio Nicolas on 2/5/20.
+//  Copyright © 2020 jn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KnobControlJN.
+FOUNDATION_EXPORT double KnobControlJNVersionNumber;
+
+//! Project version string for KnobControlJN.
+FOUNDATION_EXPORT const unsigned char KnobControlJNVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KnobControlJN/PublicHeader.h>
+
+
